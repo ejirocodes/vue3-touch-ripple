edbbd989cab00ff5eb974d6de1c073cc4ccd9de6
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2021-12-30 03:09:27 +0100

    add lib

---
+++ apps/lib/dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Vue3TouchRipple from '@/vue3-touch-ripple.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    Vue3TouchRipple
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue3-touch-ripple />
+  </div>
+</template>
