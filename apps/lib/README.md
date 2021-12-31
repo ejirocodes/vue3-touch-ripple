3f1beb4c535189a845833ca024ea7f72703bef09
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2021-12-31 21:37:56 +0100

    [publish] 1.0.0

---
+++ apps/lib/README.md
@@ -0,0 +1,4 @@
+
+# Vue3-Touch-Ripple
+
+Touch ripple component for Vue 3.x.
--- apps/src-file/src/App.vue
+++ apps/src-file/src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <main>
     <div class="example">
-      <Ripple class="love" color="#fff">
-        <div class="heart">
-          <span class="text">Ejiro</span>
-        </div>
+      <Ripple color="#fff" class="button-box" :speed="2">
+        <button>Click</button>
       </Ripple>
     </div>
   </main>
@@ -22,48 +20,21 @@ export default defineComponent({
 </script>
 
 <style scoped>
+button {
+  width: auto;
+  height: 46px;
+  line-height: 46px;
+  border: none;
+  border-radius: 5px;
+  padding: 0 1rem;
+  color: #fff;
+  cursor: pointer;
+  background-color: #2ea9df;
+}
 .example {
   display: flex;
   justify-content: space-around;
   align-items: center;
   padding: 2rem;
-}
-.example .love {
-  cursor: pointer;
-}
-.example .love > .heart {
-  position: relative;
-  width: 420px;
-  height: 360px;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.example .love > .heart > .text {
-  color: white;
-  z-index: 999999;
-  font-size: 30px;
-  margin-bottom: 60px;
-  margin-left: 20px;
-  user-select: none;
-}
-.example .love > .heart:before,
-.example .love > .heart:after {
-  position: absolute;
-  content: "";
-  left: 220px;
-  top: 10px;
-  width: 220px;
-  height: 340px;
-  background: #fc2e5a;
-  border-radius: 200px 200px 0 0;
-  transform: rotate(-45deg);
-  transform-origin: 0 100%;
-}
-.example .love > .heart:after {
-  left: 0;
-  transform: rotate(45deg);
-  transform-origin: 100% 100%;
 }
 </style>
