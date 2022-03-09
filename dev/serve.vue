e0c721d7868ea9717b0263fb7ca5b99e18f5a378
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2022-03-09 22:59:29 +0100

    update kitchen sink

--- dev/serve.vue
+++ dev/serve.vue
@@ -13,6 +13,20 @@ export default defineComponent({
 <template>
   <div id="app">
     <div class="example">
+      <vue3-touch-ripple
+        class="div-box box-1"
+        :speed="2"
+        :opacity="0.3"
+        color="#999"
+        transition="cubic-bezier(0.18, 0.89, 0.32, 1.28)"
+      >
+        <div class="box">
+          <code>cubic-bezier(0.18, 0.89, 0.32, 1.28)</code>
+        </div>
+      </vue3-touch-ripple>
+    </div>
+
+    <div class="example">
       <vue3-touch-ripple class="love" color="#fff">
         <div class="heart">
           <span class="text">Love</span>
@@ -24,9 +38,20 @@ export default defineComponent({
 <style scoped>
 .example {
   display: flex;
-  justify-content: space-around;
-  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
   padding: 2rem;
+}
+.box-1 {
+  background: #36485e;
+}
+.div-box {
+  width: calc(50% - 1rem);
+  height: 14rem;
+  line-height: 14rem;
+  text-align: center;
+  color: #fff;
 }
 .example .love {
   cursor: pointer;
